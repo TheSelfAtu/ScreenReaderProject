4480f98f0af279cb25aed5f1bba13ecb1f6307a3
TheSelfAtu <r673774d72k77e@gmail.com>
2021-01-16 11:00:32 +0900

    database unique constraint set to initialize.sql

--- myapp/initdb.d/initialize.sql
+++ myapp/initdb.d/initialize.sql
@@ -9,12 +9,10 @@ DROP TABLE IF EXISTS user;
 
 CREATE TABLE user (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    is_superuser INT DEFAULT 0,
-    username VARCHAR(40),
-    password VARCHAR(255),
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    -- CONSTRAINT AK_TransactionID UNIQUE(TransactionID)   
-    -- UNIQUE[username]
+    is_superuser INT NOT NULL DEFAULT 0,
+    username VARCHAR(40) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 INSERT INTO
@@ -28,8 +26,8 @@ DROP TABLE IF EXISTS topic;
 CREATE TABLE topic (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     post_user_id INT NOT NULL,
-    title VARCHAR(100),
-    content TEXT,
+    title VARCHAR(100) NOT NULL UNIQUE,
+    content TEXT NOT NULL,
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY(post_user_id) REFERENCES user(id)
 );
@@ -40,8 +38,8 @@ DROP TABLE IF EXISTS response_to_topic;
 CREATE TABLE response_to_topic (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     topic_id INT NOT NULL,
-    resopnse_user_id INT NOT NULL,
-    content TEXT,
+    response_user_id INT NOT NULL,
+    content TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY(topic_id) REFERENCES topic(id),
     FOREIGN KEY(response_user_id) REFERENCES user(id)
